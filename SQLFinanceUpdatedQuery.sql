462ca3dce9e5470d465ca35b3ca9ecf712f435f2
KrithikaSharma <krithikasharma2129@gmail.com>
2022-04-02 11:05:46 +0530

    Add files via upload

--- SQLFinanceUpdatedQuery.sql
+++ SQLFinanceUpdatedQuery.sql
@@ -6,7 +6,7 @@ use Finance
 create table [Admin](Aid int primary key identity, name nvarchar(30), username nvarchar(30), email nvarchar(30), [password] nvarchar(30), mobNo numeric(10))
 
 -- Customer table
-create table Customer(CusNO int primary key identity,[Name] nvarchar(30), PhnNO numeric(10) unique, email nvarchar(40) unique, username varchar(20), [Password] nvarchar(20),CPassword nvarchar(20))
+create table Customer(CusNO int primary key identity,[Name] nvarchar(30) not null, PhnNO numeric(10) unique  not null, email nvarchar(40) unique  not null, username varchar(20)  not null, [Password] nvarchar(20)  not null,CPassword nvarchar(20)  not null, bank varchar(10)  not null, AccNo varchar(20)  not null, ifscCode varchar(11)  not null, Dob Date constraint dob_chk check (dob<=cast('2002-01-01' as date))  not null,Cardtype varchar(1) not null)
 -- emiCardNo int foreign key (emiCardNo) references CardStatus(CardNo)
 
 -- Product table
@@ -15,8 +15,6 @@ create table Product(ProductId int primary key, ProductName nvarchar(max), Image
 -- Purchase table
 create table Purchase(BookingId int identity, ProdID int, CusNo int, EmiScheme int, PurchasedDate date, DueAmount decimal(10,2),MonthlyAmount decimal(10,2), primary key(BookingId,ProdID), foreign key (ProdId) references Product(ProductId), Foreign key(CusNo) references Customer(CusNO))
 
--- BankDetails table
-create table BankDetails(CusNO int , bank varchar(10), AccNo varchar(20) primary key, ifscCode varchar(11), Dob Date constraint dob_chk check (dob<=cast('2002-01-01' as date)), foreign key (CusNo) references Customer(CusNO))
 
 -- EmiCard table
 create table EmiCard(Cardtype varchar(1) primary key, CardLimit decimal(10,2), ProcessingFee int)
@@ -33,9 +31,9 @@ create table CardStatus(CardNo int primary key identity,adminid int , CusNo int,
 insert into [Admin] values('Swathi HP','Swathi','swathi.hp@zensar.com','Swathi090',8310152090)
 
 -- insert into in Customer
-insert into Customer values('Narsimha varma',8886211822,'srnarasimhavarma.v@zensar.com','srvarma','SRNVarma822','SRNVarma822')
-insert into Customer values('Shamshuddin Nalaband',9581704870,'shamshuddin.nalband@zensar.com','ShamN','ShamNal870','ShamNal870')
-insert into Customer values('Sahir',7006812967,'sahir.bhat@zensar.com','SMBhat','SahirMB967','SahirMB967')
+insert into Customer values('Narsimha varma',8886211822,'srnarasimhavarma.v@zensar.com','srvarma','SRNVarma822','SRNVarma822','icici','7898800996545327','icici0987',cast('1999-10-11' as date),'G')
+insert into Customer values('Shamshuddin Nalaband',9581704870,'shamshuddin.nalband@zensar.com','ShamN','ShamNal870','ShamNal870','hdfc','6386338720897878','hdfc002',cast('1998-07-19' as date),'G')
+insert into Customer values('Sahir',7006812967,'sahir.bhat@zensar.com','SMBhat','SahirMB967','SahirMB967', 'idfc','086108193267','idfc2255',cast('1999-08-23' as date),'T')
 
 -- insert into  product
 insert into Product values(51,'Samsung Microwave Oven','C:\Users\krith\OneDrive\Desktop\Finance\Product Images\Microwave oven',3,'28L, Black Color',43200.00,98200.00)
@@ -47,10 +45,6 @@ insert into Product values(201,'Mi Led tv 2c','C:\Users\krith\OneDrive\Desktop\F
 insert into Purchase values(201,3,6,cast(GETDATE() as date),31250.00,6250)
 insert into Purchase values(51,1,3,cast(GETDATE() as date),28800.00,14400)
 
--- insert into BankDetails
-insert into BankDetails values(1,'HDFC','6547892122326754','hdfc007','1999-10-23')
-insert into BankDetails values(2,'ICICI','2984863173868720','icici209','1998-03-12')
-insert into BankDetails values(3,'idfc','5432109983418637','idfch301','1999-07-30')
 
 -- insert into EmiCard
 insert into EmiCard values('G',100000.00,5000)
@@ -70,7 +64,6 @@ select * from [Admin]
 select * from Customer
 select * from Product
 select * from Purchase
-select * from BankDetails
 select * from EmiCard
 select * from TransactionHistory
 select * from CardStatus
@@ -81,6 +74,22 @@ select * from CardStatus
 --drop table Customer
 --drop table Product
 --drop table Purchase
---drop table BankDetails
 --drop table EmiCard
 --drop table TransactionHistory
+--drop table CardStatus
+
+
+
+
+
+
+
+
+
+
+-- BankDetails table
+--create table BankDetails(CusNO int , bank varchar(10), AccNo varchar(20) primary key, ifscCode varchar(11), Dob Date constraint dob_chk check (dob<=cast('2002-01-01' as date)), foreign key (CusNo) references Customer(CusNO))
+-- insert into BankDetails
+--insert into BankDetails values(1,'HDFC','6547892122326754','hdfc007','1999-10-23')
+--insert into BankDetails values(2,'ICICI','2984863173868720','icici209','1998-03-12')
+--insert into BankDetails values(3,'idfc','5432109983418637','idfch301','1999-07-30')
